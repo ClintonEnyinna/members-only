39f01ed071a0af24d10e061a465cfe01033706e7
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-02-25 17:02:32 -0600

    set current user after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,25 @@
 class ApplicationController < ActionController::Base
     def log_in(user)
+      session[:user_id] = user.id
+
       update_remember_token(user)
       cookies.permanent.signed[:user_id] = user.id
       cookies.permanent[:remember_token] = user.token
     end
 
     def current_user?(user)
-      current_user = user
+        user == current_user
     end
 
     def current_user
-      if (user_id = cookies.signed[:user_id])
+      if (user_id = session[:user_id])
+        @current_user ||= User.find_by(id: user_id)
+      elsif (user_id = cookies.signed[:user_id])
         user = User.find_by(id: user_id)
         if user && user.authenticated?(cookies[:remember_token])
+          log_in user
           @current_user = user
         end
-      else
-        @current_user ||= User.find_by(id: user_id)
       end
     end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password]))
-      current_user
       log_in @user
+      current_user
     end
   end
 
