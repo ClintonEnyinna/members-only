30b05e17f2dc57bbc8aaea4ce2f570465d60d570
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-02-25 13:49:05 -0600

    sessions_controller created / new view filled with form_for / added remember_token with migration to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
-    has_secure_password 
+    before_create :create_remember_token
+    has_secure_password
+
+    def self.new_token
+      SecureRandom.urlsafe_base64
+    end
+    
+    private
+
+    def create_remember_token
+      remember_digest = Digest::SHA1.hexdigest(User.new_token.to_s)
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_154045) do
+ActiveRecord::Schema.define(version: 2020_02_25_192129) do
 
   create_table "posts", force: :cascade do |t|
     t.text "body"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_02_25_154045) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_token"
   end
 
 end
