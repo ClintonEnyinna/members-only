e7736c7cb4b5720176e3e9b94f34a26fc3491937
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-02-28 09:14:08 -0600

    move current_user method to application_helper module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
+
   def log_in(user)
     session[:user_id] = user.id
 
@@ -19,18 +21,6 @@ class ApplicationController < ActionController::Base
 
   def current_user_is(user)
     @current_user = user
-  end
-
-  def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user&.authenticated?(cookies[:remember_token])
-        log_in user
-        current_user_is(user)
-      end
-    end
   end
 
   private
